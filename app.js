fcdbad04f6d916d6d59d8b31e6e524ce2c3fd680
Tayser Mohamed <tayser.mohamed1@gmail.com>
2021-08-03 05:45:18 +0300

    add contact-us route

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res, next) {
 	res.send('Homepage');
 });
 
+app.get('/contact-us', function(req, res, next) {
+	res.send('Contact us page');
+});
+
 app.listen(3000, () => console.log('server is running on port 3000..'));
