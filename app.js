91d7fd0c85a3ff4d8298e4d8bc3ac67e3bc472c5
Tayser Mohamed <tayser.mohamed1@gmail.com>
2021-08-03 05:47:05 +0300

    add about route

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get('/contact-us', function(req, res, next) {
 	res.send('Contact us page');
 });
 
+app.get('/about', function(req, res, next) {
+	res.send('About page');
+});
+
 app.listen(3000, () => console.log('server is running on port 3000..'));
